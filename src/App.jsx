24299e35ebf0a4295a5c913aa6b0c4283050832f
Juan Carlos <jc.gallardo@ocasionplus.com>
2022-07-27 08:11:54 +0200

    design aproximation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import "./App.css"
+
+const numbers = (
+    <div className="numbers">
+    {
+        Array(10).fill(null).map((value, index) => (
+            <button>{ (index + 1) % 10 }</button>
+        ))
+    }
+    </div>
+);
+
+const functions = (
+    <div className="functions">
+        <button>Clear</button>
+        <button>r</button>
+    </div>
+);
+
+const mathOperations = (
+    <div className="math-operations">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+    </div>
+);
+
 
 const App = () => (
-    <h1>Calculadora React</h1>
+    <div className="calculadora">
+        <h1>Calculadora</h1>
+        <div>
+            { numbers }
+            { functions }
+        </div>
+        { mathOperations }
+    </div>
 )
 
 export default App;
