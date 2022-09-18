e873397019cc87387400665e27d7aaa3e9b30a7f
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-09-18 12:58:02 +0200

    update render method for react18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import ReactDOM from "react-dom";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+import { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+function AppWithCallbackAfterRender() {
+    useEffect(() => {
+      console.log('App rendered!');
+    });
+  
+    return <App />
+  }
+  
+  const container = document.getElementById("app");
+  const root = createRoot(container);
+  root.render(<AppWithCallbackAfterRender />);
